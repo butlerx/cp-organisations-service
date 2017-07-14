1750f81a106c0a31ca1ba6062c444d5e18c23ce7
Wardormeur <wardormeur@gmail.com>
2017-07-14 10:54:44 +0100

    Remove this beautiful hacked view for user-org :(

--- database/pg/migrations/01.do.base-table.sql
+++ database/pg/migrations/01.do.base-table.sql
@@ -18,26 +18,3 @@ CREATE TABLE IF NOT EXISTS cd_user_org (
 ) WITH (
   OIDS=FALSE
 );
-
-CREATE OR REPLACE FUNCTION wait_user_table() RETURNS VOID AS $$
-DECLARE
-  counted NUMERIC;
-BEGIN
-  SELECT count(*)
-    FROM information_schema.tables INTO counted
-    WHERE table_name = 'cd_user_profile';
-  RAISE NOTICE 'Counter(%)', counted;
-  IF counted > 0 THEN
-    CREATE OR REPLACE VIEW cd_v_user_org AS
-      SELECT up.user_id as user_id, up.name as username, user_type, o.id as org_id, o.name as org_name
-      FROM cd_user_org uo
-      JOIN cd_organisations o ON uo.org_id = o.id
-      JOIN cd_user_profile up ON uo.user_id = up.user_id;
-  ELSE
-    PERFORM pg_sleep(1);
-    RAISE NOTICE 'Slept';
-	  PERFORM wait_user_table();
-  END IF;
-END; $$ LANGUAGE 'plpgsql';
-
-SELECT wait_user_table();
