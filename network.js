b857acf5191650657116d71e989af8d0560078c6
butlerx <butlerx@notthe.cloud>
2017-07-24 12:40:39 +0100

    add dockerfile and kube deploy

--- network.js
+++ network.js
@@ -2,9 +2,13 @@
 
 module.exports = function (seneca) {
   seneca.listen()
-    .client({type: 'web', port: 10303, pin: {role: 'cd-users', cmd: '*'}})
-    .client({type: 'web', port: 10303, pin: {role: 'cd-profiles', cmd: '*'}})
-    .client({type: 'web', port: 10303, pin: {role: 'cd-user-profile', cmd: '*'}})
-    .client({type: 'web', port: 10306, pin: {role: 'cd-events', cmd: '*'}})
-    .client({type: 'web', port: 10301, pin: {role: 'cd-dojos', cmd: '*'}});
+    .client({
+      type: 'web',
+      host: process.env.CD_USERS || 'localhost',
+      port: 10303,
+      pin: {
+        role: 'cd-profiles',
+        cmd: '*'
+      }
+    });
 };
