23023abd6d0bcf782cc8235e28ed9ae4d6c68c2a
Cian Butler <butlerx@notthe.cloud>
2017-09-04 10:50:12 +0100

    update to circle v2 (#11)
    * update to circle v2

--- deploy/start-server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-isExistApp=`ps -eaf |grep cp-organisations-service |grep -v grep| awk '{ print $2; }'`
-if [[ -n $isExistApp ]]; then
-    service cp-organisations-service stop
-fi
-
-service cp-organisations-service start
--- deploy/stop-server.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-isExistApp=`pgrep cp-organisations-service`
-if [[ -n $isExistApp ]]; then
-  service cp-organisations-service stop
-fi
