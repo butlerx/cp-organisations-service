0435490c42db085258af61cd7f94756841199707
butlerx <butlerx@notthe.cloud>
2017-12-13 12:31:33 +0000

    add rabbit mq transport

--- imports.js
+++ imports.js
@@ -12,10 +12,21 @@ module.exports = (configOverride) => {
   const log = cpLogs({ name: 'cp-organisations', level: 'warn' });
   config.log = log.log;
   const server = seneca(_.extend(config, configOverride));
-  server.use(senecaEntity).use(senecaBasic).use(senecaJoi);
-  server.use('./cd-organisations', {});
-  server.use(cpPerm, {
-    config: path.resolve(`${__dirname}/lib/organisations/controllers/perm`),
-  });
+  server
+    .use(senecaEntity)
+    .use(senecaBasic)
+    .use(senecaJoi)
+    .use('./cd-organisations', {})
+    .use(cpPerm, {
+      config: path.resolve(`${__dirname}/lib/organisations/controllers/perm`),
+    })
+    .use('seneca-amqp-transport')
+    .listen({
+      type: 'amqp',
+      pin: 'cmd:*',
+      hostname: process.env.RABBIT_HOST || 'transport',
+      user: process.env.RABBIT_USER || 'guest',
+      password: process.env.RABBIT_PASS || 'guest',
+    });
   return server;
 };
